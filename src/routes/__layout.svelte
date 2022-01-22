865fc198f02a720da07fd7d7bf420b575389cdce
Matej <matejchalachan@gmail.com>
2022-01-22 22:56:49 +0100

    added error component and laoding component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,14 @@
 <script>
+	import { navigating } from '$app/stores';
 	import Header from '$lib/header/Header.svelte';
 	import '../app.css';
 	import Footer from "$lib/footer/Footer.svelte";
+	import LoadingIndicator from "$lib/LoadingIndicator.svelte";
 </script>
+
+{#if $navigating }
+	<LoadingIndicator />
+{/if}
 
 <Header />
 
