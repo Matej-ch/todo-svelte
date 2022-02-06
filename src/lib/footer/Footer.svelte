aafcf8271033d52b7ee6c1282e6f8d22254cfd9a
Matej <matejchalachan@gmail.com>
2022-02-06 16:36:46 +0100

    update footer design

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,31 +1,20 @@
 <script>
     import Fa from 'svelte-fa/src/fa.svelte'
-    import { faFlag } from '@fortawesome/free-solid-svg-icons'
+    import { faGithub } from '@fortawesome/free-brands-svg-icons'
 </script>
 
 <footer>
-    <div class="bg-slate-900">
-        <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-            <p class="text-white text-sm text-center sm:text-left"> Developed with svelte
-                <Fa icon={faFlag} /> Flag
-                <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-slate-400 ml-1" target="_blank">@Developer</a>
-            </p>
-        </div>
+    <div class="py-2 px-2 flex flex-wrap flex-col sm:flex-row">
+        <p class="text-white text-sm flex flex-row items-center">
+            <span>Developed with svelte</span>
+            <Fa icon={faGithub}  class="ml-4"/>
+            <a href="https://github.com/Matej-ch" rel="noopener noreferrer" class="text-slate-400 ml-1" target="_blank">@Matej-ch</a>
+        </p>
     </div>
 </footer>
 
 <style>
     footer {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding: 40px;
-    }
-
-    @media (min-width: 480px) {
-        footer {
-            padding: 40px 0;
-        }
+        @apply bg-slate-900 flex flex-col justify-start p-5;
     }
 </style>
