6c15e6995ff6f5a7ebe36af4c2b2e1a6d79c83c3
Matej <matejchalachan@gmail.com>
2022-02-20 16:38:40 +0100

    alignemnt theme picker

--- src/routes/themes/index.svelte
+++ src/routes/themes/index.svelte
@@ -1,13 +1,38 @@
 <script>
 
-    export const themes = ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk"];
+    export const themes = ["light",
+        "dark",
+        "cupcake",
+        "bumblebee",
+        "emerald",
+        "corporate",
+        "synthwave",
+        "retro",
+        "cyberpunk",
+        "valentine",
+        "halloween",
+        "garden",
+        "forest",
+        "aqua",
+        "lofi",
+        "pastel",
+        "fantasy",
+        "wireframe",
+        "black",
+        "luxury",
+        "dracula",
+        "cmyk"];
     import {theme} from '$lib/stores'
 </script>
 
-<div class="flex items-center gap-1 flex-row flex-wrap">
-    <select class="select w-full max-w-md" bind:value="{$theme}">
-        {#each themes as theme }
-            <option value="{theme}">{theme}</option>
-        {/each}
-    </select>
+<div class="flex items-center gap-1 flex-row flex-wrap justify-center p-10">
+    <label>
+        Pick a theme
+        <select class="select w-full select-primary max-w-md" bind:value="{$theme}">
+            {#each themes as theme }
+                <option value="{theme}">{theme}</option>
+            {/each}
+        </select>
+    </label>
+
 </div>
